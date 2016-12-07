92e95aa73e77ba9080d85a16efc01f71d074ee0f
Ryan Havens <ryan.d.havens@gmail.com>
2016-12-07 02:04:15 -0500

    Run this file to validate output

--- check_output.py
+++ check_output.py
@@ -1,7 +1,18 @@
 from collections import defaultdict
+from optparse import OptionParser
 import os
 
-right = open('right.txt','r')
+def parse_opts():
+        parser = OptionParser()
+        parser.add_option('-d', '--datafile', dest='datafile',
+                          help='file to check against', type='string', default='right.txt')
+        return parser.parse_args()
+
+options, args = parse_opts()
+
+print('Checking files in output against canonical file %s' % options.datafile)
+
+right = open(options.datafile,'r')
 vocabulary = defaultdict(lambda: 0)
 
 for line in right:
@@ -15,10 +26,10 @@ for filename in os.listdir('output'):
             out_count += 1
             line = line.strip().split('\t')
             if not vocabulary[line[0]]:
-                print("%s was not found in canonical text")
+                print('%s was not found in canonical text')
             elif (vocabulary[line[0]] != int(line[1])):
-                print("%s: Expected %d, found %d" % (line[0], vocabulary[line[0]], line[1]))
+                print('%s: Expected %d, found %d' % (line[0], vocabulary[line[0]], line[1]))
 
-print("%d words in canonical, %d words in output" % (len(vocabulary.keys()), out_count))
-print("Done")
+print('%d words in canonical, %d words in output' % (len(vocabulary.keys()), out_count))
+print('Done')
 
