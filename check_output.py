207814f53d08c8a878e9a46de352391c7d5a9f39
Ryan Havens <ryan.d.havens@gmail.com>
2016-12-07 13:53:53 -0500

    Fixed bug in check_output.py

--- check_output.py
+++ check_output.py
@@ -13,6 +13,7 @@ options, args = parse_opts()
 print('Checking files in output against canonical file %s' % options.datafile)
 
 right = open(options.datafile,'r')
+outfile = open('compiled_output.txt','w')
 vocabulary = defaultdict(lambda: 0)
 
 for line in right:
@@ -23,13 +24,15 @@ out_count = 0
 for filename in os.listdir('output'):
     with open('output/' + filename,'r') as f:
         for line in f:
+            outfile.write(line)
             out_count += 1
             line = line.strip().split('\t')
             if not vocabulary[line[0]]:
                 print('%s was not found in canonical text')
             elif (vocabulary[line[0]] != int(line[1])):
-                print('%s: Expected %d, found %d' % (line[0], vocabulary[line[0]], line[1]))
+                print('%s: Expected %d, found %d' % (line[0], vocabulary[line[0]], int(line[1])))
 
 print('%d words in canonical, %d words in output' % (len(vocabulary.keys()), out_count))
+print('Writing compiled output to compiled_output.txt')
 print('Done')
 
